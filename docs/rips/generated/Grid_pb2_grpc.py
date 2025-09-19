db9c82bbeec582fba08b04e1be0ebce3b9ba1715
magnesj <1793152+magnesj@users.noreply.github.com>
2025-09-19 06:10:55 +0000

    Update rips module and Python examples

--- docs/rips/generated/Grid_pb2_grpc.py
+++ docs/rips/generated/Grid_pb2_grpc.py
@@ -6,10 +6,8 @@ import warnings
 import Definitions_pb2 as Definitions__pb2
 import Grid_pb2 as Grid__pb2
 
-GRPC_GENERATED_VERSION = '1.63.2'
+GRPC_GENERATED_VERSION = '1.75.0'
 GRPC_VERSION = grpc.__version__
-EXPECTED_ERROR_RELEASE = '1.65.0'
-SCHEDULED_RELEASE_DATE = 'June 25, 2024'
 _version_not_supported = False
 
 try:
@@ -19,15 +17,12 @@ except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
-    warnings.warn(
+    raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
         + f' but the generated code in Grid_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
-        + f' This warning will become an error in {EXPECTED_ERROR_RELEASE},'
-        + f' scheduled for release on {SCHEDULED_RELEASE_DATE}.',
-        RuntimeWarning
     )
 
 
@@ -100,6 +95,7 @@ def add_GridServicer_to_server(servicer, server):
     generic_handler = grpc.method_handlers_generic_handler(
             'rips.Grid', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
+    server.add_registered_method_handlers('rips.Grid', rpc_method_handlers)
 
 
  # This class is part of an EXPERIMENTAL API.
