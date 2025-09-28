7d05599a51eb18a57b75de9da9ca1fb66dbbd5e6
magnesj <1793152+magnesj@users.noreply.github.com>
2025-09-28 06:09:30 +0000

    Update rips module and Python examples

--- docs/rips/generated/Commands_pb2_grpc.py
+++ docs/rips/generated/Commands_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import Commands_pb2 as Commands__pb2
 
-GRPC_GENERATED_VERSION = '1.75.0'
+GRPC_GENERATED_VERSION = '1.75.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
