5a69fa6dba55f149b3e38fff475c28774b054cbc
magnesj <1793152+magnesj@users.noreply.github.com>
2025-10-22 06:11:22 +0000

    Update rips module and Python examples

--- docs/rips/generated/NNCProperties_pb2_grpc.py
+++ docs/rips/generated/NNCProperties_pb2_grpc.py
@@ -7,7 +7,7 @@ import Case_pb2 as Case__pb2
 import Definitions_pb2 as Definitions__pb2
 import NNCProperties_pb2 as NNCProperties__pb2
 
-GRPC_GENERATED_VERSION = '1.75.1'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -20,7 +20,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in NNCProperties_pb2_grpc.py depends on'
+        + ' but the generated code in NNCProperties_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
--- docs/rips/generated/SimulationWell_pb2_grpc.py
+++ docs/rips/generated/SimulationWell_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import SimulationWell_pb2 as SimulationWell__pb2
 
-GRPC_GENERATED_VERSION = '1.75.1'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -18,7 +18,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in SimulationWell_pb2_grpc.py depends on'
+        + ' but the generated code in SimulationWell_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
