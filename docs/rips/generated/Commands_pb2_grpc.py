2e9185a55cf1275942be6939ad7fc85a1238ab74
magnesj <1793152+magnesj@users.noreply.github.com>
2026-07-31 07:07:49 +0000

    Update rips module and proto files

--- docs/rips/generated/Commands_pb2_grpc.py
+++ docs/rips/generated/Commands_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import Commands_pb2 as Commands__pb2
 
-GRPC_GENERATED_VERSION = '1.82.1'
+GRPC_GENERATED_VERSION = '1.83.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
